cacce47ca8423bb38a4df420c21258d0db440ca5
Ryong <kryong@neighbor21.co.kr>
2023-05-22 09:53:57 +0900

    20230522계정보안

--- assets/javascripts/loginCk.js
+++ assets/javascripts/loginCk.js
@@ -1,16 +1,12 @@
 function check(){
-  var serialNum = prompt("serial number", "need to check");
+  // var serialNum = prompt("serial number", "need to check");
   var pw = prompt("비밀번호를 입력하시기 바랍니다.", "");
-  var log = document.getElementById("openHack");
-  var going = 0;
-  if(serialNum == "2506" && pw == "sun2010"){
+  if(pw == "sun2010"){
       alert("확인되었습니다.");
-      going = 1;
-      // location.href="{{ site.paths.blog | relative_url }}"
+      location.href="/new_remember/blog/"
   }else{
-      alert("시리얼넘버나 비밀번호가 틀렸습니다. 데이터를 체크합니다.");
-      going;
-      // document.getElementById("openHack").click();
+      alert("비밀번호가 틀렸습니다. 데이터를 체크합니다.");
+      document.getElementById("openHack").click();
 
   }
 }
